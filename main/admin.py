5ab34416e02aa4fe5c1598ae0d2c8c724fd357f2
Lehi Cano <onacihel@gmail.com>
2023-02-12 17:19:41 -0500

    adds styling and image upload

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Trip, Gear
+from .models import Trip, Gear, Photo
 
 admin.site.register(Trip)
 admin.site.register(Gear)
+admin.site.register(Photo)
