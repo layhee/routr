aa21be019397e88c066742f3163c8cd44ece5ad8
Lehi Cano <onacihel@gmail.com>
2023-02-12 20:30:12 -0500

    final prep for deployment

--- routr/settings.py
+++ routr/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import django_on_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'routr.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'layhee/routr',
+        'USER': 'layhee',
+        'PASSWORD': 'v2_3z4aL_ZVpZ3LdKuqNBVCNgi3sn23T',
+        'HOST': 'db.bit.io',
+        'PORT': '5432',
     }
 }
 
@@ -124,3 +129,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_on_heroku.settings(locals())
