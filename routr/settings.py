3755c3e43983f947868da94f9b3d317245692ef5
Lehi Cano <onacihel@gmail.com>
2023-02-13 19:11:54 -0500

    wip

--- routr/settings.py
+++ routr/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv("NAME"),
         'USER': os.getenv("USER"),
-        'PASSWORD': os.getenv("API_PASSWORD"),
+        'PASSWORD': os.getenv("PASSWORD"),
         'HOST': os.getenv("HOST"),
         'PORT': os.getenv("PORT"),
         'HOST': os.getenv("HOST")
