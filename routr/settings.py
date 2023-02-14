83a52e0436fe161bea7225c1f0d487b36fd8e549
Lehi Cano <onacihel@gmail.com>
2023-02-13 21:35:13 -0500

    wip

--- routr/settings.py
+++ routr/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-LOGIN_REDIRECT_URL = '/trips'
+LOGIN_REDIRECT_URL = '/trips/'
 LOGOUT_REDIRECT_URL = '/'
 STATIC_URL = 'static/'
 STATIC_ROOT = 'static'
